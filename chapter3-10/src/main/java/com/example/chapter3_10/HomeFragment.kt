59cafbdd1afc18331a666ed502944bce95d02d8e
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-03-19 23:47:40 +0900

    fragment활용 및 navigation활용 진행중...

--- chapter3-10/src/main/java/com/example/chapter3_10/HomeFragment.kt
+++ chapter3-10/src/main/java/com/example/chapter3_10/HomeFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.chapter3_10.data.ArticleModel
+import com.example.chapter3_10.databinding.FragmentHomeBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
 
+    private lateinit var binding : FragmentHomeBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
 
         val db = Firebase.firestore
 
@@ -21,6 +28,19 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
             Log.e("homeFragment",article.toString())
         }.addOnFailureListener {
             it.printStackTrace()
+        }
+        
+        setUpWriteButton(view)
+    }
+
+    private fun setUpWriteButton(view: View) {
+        binding.writeButton.setOnClickListener {
+            if(Firebase.auth.currentUser != null) {
+                val action = HomeFragmentDirections.actionHomeFragmentToWriteArticleFragment()
+                findNavController().navigate(action)
+            } else {
+                Snackbar.make(view, "로그인 후 사용해주세요",Snackbar.LENGTH_SHORT).show()
+            }
         }
     }
 }
---
+++ chapter3-10/src/main/java/com/example/chapter3_10/ui/article/WriteArticleFragment.kt
@@ -0,0 +1,72 @@
+package com.example.chapter3_10.ui.article
+
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.chapter3_10.R
+import com.example.chapter3_10.databinding.FragmentWriteBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.util.UUID
+
+class WriteArticleFragment: Fragment(R.layout.fragment_write) {
+
+    private lateinit var binding: FragmentWriteBinding
+
+    private var selectedUri: Uri ?= null
+    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        // Callback is invoked after the user selects a media item or closes the
+        // photo picker.
+        if (uri != null) {
+            selectedUri = uri
+            binding.photoImageView.setImageURI(uri)
+        } else {
+            Log.d("PhotoPicker", "No media selected")
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentWriteBinding.bind(view)
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+
+        binding.photoImageView.setOnClickListener {
+
+        }
+
+        binding.deleteButton.setOnClickListener {
+
+        }
+
+        binding.submitButton.setOnClickListener {
+            if(selectedUri != null) {
+                val photoUri = selectedUri ?: return@setOnClickListener
+                val fileName = "${UUID.randomUUID()}.png"
+                Firebase.storage.reference.child("articles/photo").child(fileName).putFile(photoUri).addOnCompleteListener{task->
+                    if(task.isSuccessful) {
+                        Firebase.storage.reference.child("articles/photo").child(fileName).downloadUrl.addOnSuccessListener {
+                            Log.e("WriteArticleFragment",it.toString())
+                        }.addOnFailureListener {
+
+                        }
+                    } else {
+                        //error
+                    }
+                }
+            } else {
+                Snackbar.make(view,"이미지가 선택되지 않았습니다.",Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(WriteArticleFragmentDirections.actionBack())
+        }
+    }
+}
