0852c7a2143da0302106fff68466fc87c375a06d
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-03-19 19:52:21 +0900

    fragment활용 및 navigation활용 진행중...

---
+++ chapter3-10/src/main/java/com/example/chapter3_10/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.chapter3_10
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.chapter3_10.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        binding.bottomNavigationView.setupWithNavController(navHostFragment.navController)
+    }
+
+
+}
